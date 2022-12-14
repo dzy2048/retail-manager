db4ca09fa0255d9f808bc7a90bcf1b2f104c0964
dzy2048 <bx21dzy@163.com>
2022-12-14 12:35:17 +0800

    导航栏在未登录时禁用

--- vue/src/components/Aside.vue
+++ vue/src/components/Aside.vue
@@ -1,63 +1,71 @@
 <template>
-  <el-row class="tac">
-    <el-col :span="18">
-      <el-menu
-        default-active="3"
-        router
-        class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose">
-        <el-menu-item index="/index/sales">
-          <i class="el-icon-document-remove"></i>
-          <span slot="title">开销售单</span>
-        </el-menu-item>
-        <el-menu-item index="/index/order">
-          <i class="el-icon-menu"></i>
-          <span slot="title">订单管理</span>
-        </el-menu-item>
-        <el-menu-item index="/index/customer">
-          <i class="el-icon-message"></i>
-          <span slot="title">客户信息</span>
-        </el-menu-item>
-        <el-menu-item index="/index/good">
-          <i class="el-icon-menu"></i>
-          <span slot="title">商品管理</span>
-        </el-menu-item>
-        <el-menu-item index="/index/repository">
-          <i class="el-icon-document"></i>
-          <span slot="title">库存管理</span>
-        </el-menu-item>
-        <el-menu-item index="/index/dispatch">
-          <i class="el-icon-setting"></i>
-          <span slot="title">库存调度</span>
-        </el-menu-item>
-      </el-menu>
-    </el-col>
-  </el-row>
+    <el-row class="tac">
+        <el-col :span="18">
+            <el-menu
+                default-active="3"
+                router
+                class="el-menu-vertical-demo"
+                @open="handleOpen"
+                @close="handleClose">
+                <el-menu-item index="/index/sales" :disabled="!isLogin">
+                    <i class="el-icon-document-remove"></i>
+                    <span slot="title">开销售单</span>
+                </el-menu-item>
+                <el-menu-item index="/index/order" :disabled="!isLogin">
+                    <i class="el-icon-menu"></i>
+                    <span slot="title">订单管理</span>
+                </el-menu-item>
+                <el-menu-item index="/index/customer" :disabled="!isLogin">
+                    <i class="el-icon-message"></i>
+                    <span slot="title">客户信息</span>
+                </el-menu-item>
+                <el-menu-item index="/index/good" :disabled="!isLogin">
+                    <i class="el-icon-menu"></i>
+                    <span slot="title">商品管理</span>
+                </el-menu-item>
+                <el-menu-item index="/index/repository" :disabled="!isLogin">
+                    <i class="el-icon-document"></i>
+                    <span slot="title">库存管理</span>
+                </el-menu-item>
+                <el-menu-item index="/index/dispatch" :disabled="!isLogin">
+                    <i class="el-icon-setting"></i>
+                    <span slot="title">库存调度</span>
+                </el-menu-item>
+            </el-menu>
+        </el-col>
+    </el-row>
 </template>
 
 <script>
 export default {
-  name: "Aside",
-  methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
+    name: "Aside",
+    data() {
+        return  {
+            isLogin: false
+        }
     },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
+    methods: {
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+            console.log(key, keyPath);
+        }
+    },
+    created() {
+        if (sessionStorage.getItem('userName')!=null)
+            this.isLogin = true
     }
-  }
 }
 </script>
 
 <style scoped>
-  .el-menu
-  {
+.el-menu {
     min-height: calc(100vh - 60px);
-  }
-  .router-link
-  {
+}
+
+.router-link {
     text-decoration: none;
-  }
+}
 
 </style>
