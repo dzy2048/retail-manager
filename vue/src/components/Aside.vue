93d6d29420d65bb4ad71990c3e34e6ad54c87f34
dzy2048 <bx21dzy@163.com>
2022-12-18 15:00:52 +0800

    设置了管理员和非管理员的权限

--- vue/src/components/Aside.vue
+++ vue/src/components/Aside.vue
@@ -11,23 +11,23 @@
                     <i class="el-icon-document-remove"></i>
                     <span slot="title">开销售单</span>
                 </el-menu-item>
-                <el-menu-item index="/index/order" :disabled="!isLogin">
+                <el-menu-item index="/index/order" :disabled="!isLogin || !isAuthority">
                     <i class="el-icon-menu"></i>
                     <span slot="title">订单管理</span>
                 </el-menu-item>
-                <el-menu-item index="/index/customer" :disabled="!isLogin">
+                <el-menu-item index="/index/customer" :disabled="!isLogin || !isAuthority">
                     <i class="el-icon-message"></i>
                     <span slot="title">客户信息</span>
                 </el-menu-item>
-                <el-menu-item index="/index/good" :disabled="!isLogin">
+                <el-menu-item index="/index/good" :disabled="!isLogin || !isAuthority">
                     <i class="el-icon-menu"></i>
                     <span slot="title">商品管理</span>
                 </el-menu-item>
-                <el-menu-item index="/index/repository" :disabled="!isLogin">
+                <el-menu-item index="/index/repository" :disabled="!isLogin || !isAuthority">
                     <i class="el-icon-document"></i>
                     <span slot="title">库存管理</span>
                 </el-menu-item>
-                <el-menu-item index="/index/dispatch" :disabled="!isLogin">
+                <el-menu-item index="/index/dispatch" :disabled="!isLogin || !isAuthority">
                     <i class="el-icon-setting"></i>
                     <span slot="title">库存调度</span>
                 </el-menu-item>
@@ -46,6 +46,7 @@ export default {
     props: ['isLogin'],
     data() {
         return  {
+            isAuthority: sessionStorage.getItem("authority") === '1'
         }
     },
     methods: {
@@ -57,7 +58,6 @@ export default {
         }
     },
     created() {
-
     }
 }
 </script>
