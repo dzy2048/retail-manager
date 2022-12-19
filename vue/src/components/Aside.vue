1e5e8f41bcb10cd04ca3a09bbc3fa5a59d427a2e
dzy2048 <bx21dzy@163.com>
2022-12-19 19:59:16 +0800

    修改了导航栏的图标

--- vue/src/components/Aside.vue
+++ vue/src/components/Aside.vue
@@ -8,31 +8,31 @@
                 @open="handleOpen"
                 @close="handleClose">
                 <el-menu-item index="/index/sales" :disabled="!isLogin">
-                    <i class="el-icon-document-remove"></i>
+                    <i class="el-icon-document-add"></i>
                     <span slot="title">开销售单</span>
                 </el-menu-item>
                 <el-menu-item index="/index/order" :disabled="!isLogin || !isAuthority">
-                    <i class="el-icon-menu"></i>
+                    <i class="el-icon-s-order"></i>
                     <span slot="title">订单管理</span>
                 </el-menu-item>
                 <el-menu-item index="/index/customer" :disabled="!isLogin || !isAuthority">
-                    <i class="el-icon-message"></i>
+                    <i class="el-icon-s-custom"></i>
                     <span slot="title">客户信息</span>
                 </el-menu-item>
                 <el-menu-item index="/index/good" :disabled="!isLogin || !isAuthority">
-                    <i class="el-icon-menu"></i>
+                    <i class="el-icon-s-goods"></i>
                     <span slot="title">商品管理</span>
                 </el-menu-item>
                 <el-menu-item index="/index/repository" :disabled="!isLogin || !isAuthority">
-                    <i class="el-icon-document"></i>
+                    <i class="el-icon-s-home"></i>
                     <span slot="title">库存管理</span>
                 </el-menu-item>
                 <el-menu-item index="/index/dispatch" :disabled="!isLogin || !isAuthority">
-                    <i class="el-icon-setting"></i>
+                    <i class="el-icon-sort"></i>
                     <span slot="title">库存调度</span>
                 </el-menu-item>
                 <el-menu-item index="/index/statistics" :disabled="!isLogin">
-                    <i class="el-icon-setting"></i>
+                    <i class="el-icon-s-data"></i>
                     <span slot="title">数据统计</span>
                 </el-menu-item>
             </el-menu>
